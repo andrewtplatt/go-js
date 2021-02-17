ce8ed66ae4a57c1e33f1a15e79f71cb60914cd35
Andrew Platt <andrew.platt@st-annes.ox.ac.uk>
2021-02-17 14:39:36 +0000

    added goJS and firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBWyZz0y45PaLxyIEymjBatecunk6SQ_M",
+  authDomain: "vectis-gojs.firebaseapp.com",
+  projectId: "vectis-gojs",
+  storageBucket: "vectis-gojs.appspot.com",
+  messagingSenderId: "834367723407",
+  appId: "1:834367723407:web:8e72b9d6cdaa9d20931dfa",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
